92d2390102f1443828bbd86ba4e886367e4d62a9
Hullrf <davidsantiagorc@outlook.com>
2026-04-15 08:24:00 -0500

    feat: ruta raíz apunta a landing page (vista mínima)

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>ASAPP</title>
+</head>
+<body>
+<p>ASAPP</p>
+<a href="{{ route('register') }}">Registrar</a>
+</body>
+</html>
